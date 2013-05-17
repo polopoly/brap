4f69babd891bfcdd2f44f81d71bd6fd8f1ab5e14
edvin <edvin@9f856da2-7ffc-11de-b80a-f3367b01929d>
2013-05-17 20:05:43 +0000

    Added support for Tornado Inject
    git-svn-id: http://brap.subversion.no/brap/trunk/brap@188 9f856da2-7ffc-11de-b80a-f3367b01929d

--- brap-server/src/main/java/no/tornado/brap/servlet/InjectProxyServlet.java
+++ brap-server/src/main/java/no/tornado/brap/servlet/InjectProxyServlet.java
@@ -1,8 +1,22 @@
 package no.tornado.brap.servlet;
 
+import no.tornado.brap.auth.AuthenticationProvider;
+import no.tornado.brap.auth.AuthorizationProvider;
 import no.tornado.inject.ApplicationContext;
 
 public class InjectProxyServlet extends ProxyServlet {
+
+    public void createServiceWrapper() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        super.createServiceWrapper();
+
+        String authenticationProviderBean = servletConfig.getInitParameter("authenticationProviderBeanName");
+        if (authenticationProviderBean != null)
+            serviceWrapper.setAuthenticationProvider((AuthenticationProvider) ApplicationContext.getBean("authenticationProviderBean"));
+
+        String authorizationProviderBean = servletConfig.getInitParameter("authorizationProviderBeanName");
+        if (authorizationProviderBean != null)
+            serviceWrapper.setAuthorizationProvider((AuthorizationProvider) ApplicationContext.getBean("authorizationProviderBean"));
+    }
 
     protected Object getService() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         String beanName = servletConfig.getInitParameter("beanName");
